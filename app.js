ec4ed2b2568c34086a972b0edaf0be7e9c8413d5
cristiAndreiTarasi <tarasicristia@gmail.com>
2018-12-01 19:25:46 +0000

    This the saved version after PC shut down :))

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 
 const hour   = document.querySelector('.fore .hour'),
-    minute   = document.querySelector('.fore .minute'),
-    second   = document.querySelector('.fore .second'),
-    clock    = document.querySelector('.clock'),
-    screen   = document.querySelector('.screen');
+minute   = document.querySelector('.fore .minute'),
+second   = document.querySelector('.fore .second'),
+clock    = document.querySelector('.clock'),
+screen   = document.querySelector('.screen');
 
+//Clock
 function clockMechanism() {
     //get hour,minute and second
     let d = new Date(),
@@ -31,10 +32,33 @@ function clockMechanism() {
     setInterval(clockMechanism, 1000)
 }
 
-//swap css  sheets  at pressing on the theme swap button
- document.getElementById('theme').addEventListener('click', () => {
-    let  sheet = document.styleSheets[1];
-    sheet.disabled ? sheet.disabled  =  false : sheet.disabled = true;
+//Toggle Theme --> swap css  sheets  at pressing on the theme swap button
+document.getElementById('theme').addEventListener('click', () => {
+let  sheet = document.styleSheets[1];
+sheet.disabled ? sheet.disabled  =  false : sheet.disabled = true;
 }, false);
 
+//days highlight functionality
+//make the current day take a light color
+function currDayColor() {
+    //grab every p elem in .days div 
+    let  days = document.querySelectorAll('.days > div p');
 
+    //associate every day name in day array with days numbers returned by getDay() method
+    //and return the today variable wich holds the getDay() value hence the day it corresponds in the day array
+    let date  = new Date();
+    let day   = [ 'sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat' ];
+    let today = day[date.getDay()];
+
+    //find wich day name in .days div matches with the string returned by today variable
+    //and change its color
+    for(let i = 0, len = days.length; i < len; i++) {
+        if(days[i].textContent == today)
+            days[i].style.color = '#ddd';
+    }
+}
+
+//Stopwatch
+document.getElementById('stop').addEventListener('click', () => {
+
+}, false);
